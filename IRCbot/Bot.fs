a8d284f3ffe01d4572b7f7eeccf5598c6fa01e8c
Santiago Albo <santialbo@gmail.com>
2013-03-05 14:20:38 -0500

    Added SimpleBot and a bot example in main Program

--- IRCbot/Bot.fs
+++ IRCbot/Bot.fs
@@ -3,7 +3,6 @@ namespace IRCbot
 
 open System
 open System.IO
-open System.Net
 open System.Net.Sockets
 
 type Connection(server: string, port: int, nick: string) =
@@ -12,26 +11,53 @@ type Connection(server: string, port: int, nick: string) =
     
     let getMessage (line: string) = line.Substring(line.IndexOf(":", 1) + 1) 
     
-    member x.Connect =
+    member this.Connect() =
         client.Connect(server, port)
-        let writer = new StreamWriter(client.GetStream())
         let reader = new StreamReader(client.GetStream())
+        let writer = new StreamWriter(client.GetStream())
         writer.AutoFlush <- true
         
-        // Connection to the server
-        writer.WriteLine (sprintf "USER %s %s %s %s" nick nick nick nick)
-        writer.WriteLine (sprintf "NICK %s" nick)
-        
-        let rec connect_ = fun () ->
+        let getLine = fun () ->
             let line = reader.ReadLine()
-            printfn "%s" line
-            if line.StartsWith "PING :"  then
-                // If server pings we pong
-                writer.WriteLine (sprintf "PONG %s" (line.Substring 5))
-            elif (getMessage line).StartsWith "Welcome" then
-                () // If server welcomes we are connected.
-            else connect_()
-        do connect_()
+            printfn "< %s" line
+            line
         
-        (writer, reader)
-           
+        let putLine (line: string) =
+            writer.WriteLine line
+            printfn "> %s" line
+            
+        do
+            // Connect to the server
+            putLine (sprintf "USER %s %s %s %s" nick nick nick nick)
+            putLine (sprintf "NICK %s" nick)
+            
+            let rec connect_ = fun () ->
+                let line = getLine()
+                if line.StartsWith "PING :" then
+                    putLine (sprintf "PONG %s" (line.Substring 5)) // If server pings we pong
+                if (getMessage line).StartsWith "Welcome" then () // If server welcomes we are done
+                else connect_()
+            do connect_()
+        
+        let connected = fun () -> not reader.EndOfStream
+        
+        (getLine, putLine, connected)
+    
+    member x.Close() =
+        client.Close()
+        
+
+type SimpleBot(server: string, port: int, nick: string, channels: string list,
+               msgHandler: string -> (string -> unit) -> unit) =
+    
+    let conn = new Connection(server, port, nick)
+    
+    member this.Start = fun () ->
+        let read, write, connected = conn.Connect()
+        channels
+        |> List.iter (fun channel -> write (sprintf "JOIN %s" channel))
+        
+        while (connected()) do
+            msgHandler (read()) write
+
+
--- IRCbot/Program.fs
+++ IRCbot/Program.fs
@@ -2,10 +2,30 @@
 module IRCbot.Main
 
 open IRCbot
+open System.Text.RegularExpressions
+  
+let (|Match|_|) (pat:string) (inp:string) =
+    let m = Regex.Match(inp, pat) in
+    if m.Success then
+        Some (List.tail [for g in m.Groups -> g.Value])
+    else None
+
+let msgHandler (line: string) (write: string -> unit) =
+
+    let say channel text = write (sprintf "PRIVMSG %s :%s" channel text)
+    let ping what = write (sprintf "PONG :%s" what)
+    let join channel = write (sprintf "JOIN %s" channel)
+    
+    match line with
+    | Match @"^PING :(.+)$" [what] -> ping what
+    | Match @"^:[^:]+JOIN (#[^\s]+)$" [channel] -> say channel "Hello world!"
+    | _ -> ()
+    
+   
 
 [<EntryPoint>]
 let main args = 
-    let conn = new Connection("irc.quakenet.org", 6667, "itnas2")
-    let a = conn.Connect
+    let bot = new SimpleBot("irc.quakenet.org", 6667, "itnas2", ["#holaquetalsoycolosal"; "#holaquetalsoycolosal2"], msgHandler)
+    bot.Start()
     0
 
